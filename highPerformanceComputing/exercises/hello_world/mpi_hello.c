bd971726a145d1bdd943d7c47280864fc0cd3b79
luiss07 <luigi.delleva@gmail.com>
2024-10-11 09:52:53 +0200

    HPC - helloworld

---
+++ highPerformanceComputing/exercises/hello_world/mpi_hello.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <mpi.h>
+#include <string.h>
+
+const int MAX_STRING = 100;
+
+int main (void) {
+
+    // Initialize the MPI environment. The two arguments to MPI Init are not
+    // currently used by MPI implementations, but are there in case future
+    // implementations might need the arguments.
+    MPI_Init(NULL, NULL);
+
+    // number of processes
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    // rank of the process
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    printf("Hello world from process rank %d out of %d processors\n", world_rank, world_size);
+
+    MPI_Finalize();
+}
+
+// #include <mpi.h>
+// #include <stdio.h>
+// #include <string.h>
+
+// const int MAX_STRING = 100;
+
+// int main(void) {
+//   char greeting[MAX_STRING];
+//   int comm_sz;  // number of processes
+//   int my_rank;  // my process rank
+
+//   MPI_Init(NULL, NULL);
+//   MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+//   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+//   if (my_rank != 0) {
+//     sprintf(greeting, "Greetings from process %d of %d!", my_rank, comm_sz);
+//     MPI_Send(greeting, strlen(greeting) + 1, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+//   } else {
+//     printf("Greetings from process %d of %d!\n", my_rank, comm_sz);
+//     for (int q = 1; q < comm_sz; q++) {
+//       MPI_Recv(greeting, MAX_STRING, MPI_CHAR, q, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+//       printf("%s\n", greeting);
+//     }
+//   }
+
+//   MPI_Finalize();
+//   return 0;
+// }
